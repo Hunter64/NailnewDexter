725215909f2b836de1696a04cedb9df05b9feddd
Héctor López <hector.lopez@DSA-DEV2-10>
2019-08-08 11:31:22 -0500

    adding theme material, modified color accent, add color material style in layout and extend from Activity for compatibility

--- app/src/main/java/com/hector/nailnewdexter/MainActivity.kt
+++ app/src/main/java/com/hector/nailnewdexter/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.hector.nailnewdexter
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+//When modified colorAccent and add theme of material in style, watch this, because AppCompatActivity is for versions - 5
+//The theme Material is for Android >= 5
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
